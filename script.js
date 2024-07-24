5074c641f512b1e8a9d677f1acb3f8a7445a4e7f
Koushik Biswas <144088942+Koush98@users.noreply.github.com>
2024-07-25 02:44:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text", {
-    strings:["Business Intelligence Analyst","Data Analyst","Machine learning Engineer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-
-})
